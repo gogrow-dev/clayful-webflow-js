38c8843b379460d4e9d5dbc0bf45eb6ff6e8d7f7
Santiago Pais <santiago.pais@gogrow.dev>
2025-07-08 15:08:04 -0300

    Fix id of arrow

--- dashboard/educator/journals-tab.js
+++ dashboard/educator/journals-tab.js
@@ -47,40 +47,40 @@ export function fetchAndRenderJournals(journalsUrl, headers) {
     }
 
     row.innerHTML = `
-  <div class="journal-collection-item">
-    <div class="journal-column number table">
-      <p id="journal-number" class="text_l_dashboard white">
-        ${id}</p>
-    </div>
-    <div class="journal-column journal-info">
-      <img id="journal-thumbnail" loading="lazy" alt="" src="${journal.featuredImage}" class="journal-collection-image">
-      <div class="journal-name-description width100">
-        <p id="journal-title" fs-list-field="journalName" class="text_l_dashboard white">
-          ${journal.name}</p>
-        <p id="journal-desc" class="text_l_dashboard white opacity-60">
-          ${journal.description}</p>
+    <div class="journal-collection-item">
+      <div class="journal-column number table">
+        <p id="journal-number" class="text_l_dashboard white">
+          ${id}</p>
+      </div>
+      <div class="journal-column journal-info">
+        <img id="journal-thumbnail" loading="lazy" alt="" src="${journal.featuredImage}" class="journal-collection-image">
+        <div class="journal-name-description width100">
+          <p id="journal-title" fs-list-field="journalName" class="text_l_dashboard white">
+            ${journal.name}</p>
+          <p id="journal-desc" class="text_l_dashboard white opacity-60">
+            ${journal.description}</p>
+        </div>
+      </div>
+      <div class="journal-column table started">
+        <p id="journal-started-count" class="text_l_dashboard white">
+          ${journal.totalStarted}</p>
+      </div>
+      <div class="journal-column table completed">
+        <p id="journal-completed-count" class="text_l_dashboard white">
+          ${journal.totalCompleted}</p>
+      </div>
+      <div class="journal-column avg-time-spent table gap4">
+        <p id="journal-avg-time" class="text_l_dashboard white">
+          ${formattedTime}</p>
+        <p id="journal-avg-time" class="text_l_dashboard white">
+          minutes</p>
+      </div>
+      <div class="journal-column empty table">
+        <a href="#" class="button-dashboard-arrow-journal w-inline-block" id="journal-open-details">
+          <img loading="lazy" src="https://cdn.prod.website-files.com/62b25ea5deeeeae5c2f76889/685ed9c044c12bc74c8e8f66_tabler-icon-arrow-left.svg" alt="arrow pointing rigth" class="icon-dashboard-arrow-journal">
+        </a>
       </div>
     </div>
-    <div class="journal-column table started">
-      <p id="journal-started-count" class="text_l_dashboard white">
-        ${journal.totalStarted}</p>
-    </div>
-    <div class="journal-column table completed">
-      <p id="journal-completed-count" class="text_l_dashboard white">
-        ${journal.totalCompleted}</p>
-    </div>
-    <div class="journal-column avg-time-spent table gap4">
-      <p id="journal-avg-time" class="text_l_dashboard white">
-        ${formattedTime}</p>
-      <p id="journal-avg-time" class="text_l_dashboard white">
-        minutes</p>
-    </div>
-    <div class="journal-column empty table">
-      <a href="#" class="button-dashboard-arrow-journal w-inline-block">
-        <img loading="lazy" src="https://cdn.prod.website-files.com/62b25ea5deeeeae5c2f76889/685ed9c044c12bc74c8e8f66_tabler-icon-arrow-left.svg" alt="arrow pointing rigth" class="icon-dashboard-arrow-journal">
-      </a>
-    </div>
-  </div>
   `;
 
     row.querySelector("#journal-open-details").addEventListener("click", async function (e) {
